a0c5603593c2e8e661503d2790dc7627a31ed92e
Puneet Sheokand <93940651+puneet73@users.noreply.github.com>
2024-02-13 16:35:25 +0530

    main.cpp

--- src/serial/main.cpp
+++ src/serial/main.cpp
@@ -1,5 +1,13 @@
-#include <bits/stdc++.h>
-#include <cstdlib>
+/*
+ * tcpserver.c - A multithreaded TCP echo server
+ * usage: tcpserver <port>
+ *
+ * Testing :
+ * nc localhost <port> < input.txt
+ */
+#include <unistd.h>
+#include <iostream>
+#include <stdlib.h>
 #include <string>
 #include <sstream>
 #include <cstring>
@@ -7,93 +15,142 @@
 #include <sys/socket.h>
 #include <netinet/ip.h>
 #include <arpa/inet.h>
-#include <unistd.h>  // Add this line for close function
-#include <sys/types.h> 
+#include <bits/stdc++.h>
 using namespace std;
 
-const int BUFFER_SIZE = 1024;
 unordered_map<string, string> KV_data;
-pthread_mutex_t kv_mutex = PTHREAD_MUTEX_INITIALIZER;
 
 void process_connection(int sockfd);
 
 int main(int argc, char **argv)
 {
-    int port_number;
+  int portno; /* port to listen on */
 
-    if (argc != 2)
-    {
-        fprintf(stderr, "usage: %s <port>\n", argv[0]);
-        exit(1);
-    }
+  /*
+   * check command line arguments
+   */
+  if (argc != 2)
+  {
+    fprintf(stderr, "usage: %s <port>\n", argv[0]);
+    exit(1);
+  }
 
-    port_number = atoi(argv[1]);
-    cout << port_number << endl;
+  // DONE: Server port number taken as command line argument
+  portno = atoi(argv[1]);
+  std::cout << portno << std::endl;
+  int accept_sock, connection_sock;
+  struct sockaddr_in my_addr, client_addr;
 
-    int socket_connect;
-    struct sockaddr_in my_addr;
+  connection_sock = socket(AF_INET, SOCK_STREAM, 0);
 
-    socket_connect = socket(AF_INET, SOCK_STREAM, 0);
+  my_addr.sin_family = AF_INET;
+  my_addr.sin_port = htons(portno);
+  bind(connection_sock, (struct sockaddr *)&my_addr, sizeof(struct sockaddr_in));
 
-    my_addr.sin_family = AF_INET;
-    my_addr.sin_port = htons(port_number);
-    bind(socket_connect, (struct sockaddr *)&my_addr, sizeof(struct sockaddr_in));
+  listen(connection_sock, 50);
 
-    listen(socket_connect, 50);
+  int client_len = sizeof(client_addr);
 
-    while (true)
-    {
-        int socket_accept;
-        struct sockaddr_in client_addr;
-        socklen_t sz_client = sizeof(client_addr);
-
-        socket_accept = accept(socket_connect, (struct sockaddr *)&client_addr, &sz_client);
-        process_connection(socket_accept);
-        close(socket_accept);  // Close the accepted socket after processing
-    }
-
-    close(socket_connect);
+  while (true)
+  {
+    accept_sock = accept(connection_sock, (struct sockaddr *)&client_addr, (socklen_t *)&client_len);
+    process_connection(accept_sock);
+  }
+  
+  
+  close(connection_sock);
 }
 
 void process_connection(int sockfd)
 {
-    char buffer[BUFFER_SIZE];
+  char buffer[1024];
 
-    memset(buffer, 0, sizeof(buffer));
-    string additional = "";
-    string input_s;
+  memset(buffer, 0, sizeof(buffer));
+  string additional = "";
+  string temp;
+
+  while (true)
+  {
+    read(sockfd, buffer, 1024);
+    istringstream streamstring(additional + (string)buffer);
 
     while (true)
     {
-        ssize_t bytes_read = read(sockfd, buffer, BUFFER_SIZE - 1); // Ensure null-terminated buffer
-
-        if (bytes_read <= 0)
+      streamstring >> temp;
+      if (temp == "READ")
+      {
+        // cout <<"READ" <<endl;
+        streamstring >> temp;
+        if (KV_data.find(temp) == KV_data.end())
         {
-            break; // Break if no more data or an error occurs
+          const char *sendd = "NULL\n";
+          write(sockfd, sendd, strlen(sendd));
         }
-
-        buffer[bytes_read] = '\0'; // Null-terminate the buffer
-
-        istringstream streamstring(additional + (string)buffer);
-
-        while (true)
+        else
         {
-            streamstring >> input_s;
-            // ...
 
-            pthread_mutex_lock(&kv_mutex); // Lock before accessing shared data
-            // ... (perform operations on KV_data)
-            pthread_mutex_unlock(&kv_mutex); // Unlock after accessing shared data
-
-            // ...
-
-            if (streamstring.str().length() < 1)
-            {
-                additional = "";
-                break;
-            }
-
-            cout << input_s << endl;
+          string se = KV_data[temp] + "\n";
+          const char *sendd = se.c_str();
+          write(sockfd, sendd, strlen(sendd));
         }
+      }
+      else if (temp == "WRITE")
+      {
+        streamstring >> temp;
+        string key = temp;
+        streamstring >> temp;
+        temp = temp.substr(1, temp.length() - 1);
+        KV_data[key] = temp;
+        const char *sendd = "FIN\n";
+        write(sockfd, sendd, strlen(sendd));
+      }
+      else if (temp == "COUNT")
+      {
+        // cout <<"COUNT" <<endl;
+        int count = KV_data.size();
+        string se = to_string(count) + "\n";
+        const char *sendd = se.c_str();
+        write(sockfd, sendd, strlen(sendd));
+      }
+      else if (temp == "DELETE")
+      {
+        // cout <<"DELETE" <<endl;
+        streamstring >> temp;
+        if (KV_data.find(temp) == KV_data.end())
+        {
+
+          const char *sendd = "NULL\n";
+          write(sockfd, sendd, strlen(sendd));
+        }
+        else
+        {
+          KV_data.erase(temp);
+          const char *sendd = "FIN\n";
+          write(sockfd, sendd, strlen(sendd));
+        }
+      }
+      else if (temp == "END")
+      {
+        write(sockfd , "\n", strlen("\n"));
+        close(sockfd);
+        return;
+      }
+      else
+      {
+        if (streamstring.str().length() < 1)
+        {
+          additional = temp;
+          break;
+        }
+      }
+
+      if (streamstring.str().length() < 1)
+      {
+        additional = "";
+        break;
+      }
+
+      cout << temp << endl;
     }
+  }
 }
