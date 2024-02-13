2a93ddd6e4d0dac5665900e83ac0e803d6d3f52b
Puneet Sheokand <93940651+puneet73@users.noreply.github.com>
2024-02-13 16:38:09 +0530

    main.cpp

--- src/serial/main.cpp
+++ src/serial/main.cpp
@@ -1,156 +1,217 @@
-/*
- * tcpserver.c - A multithreaded TCP echo server
- * usage: tcpserver <port>
- *
- * Testing :
- * nc localhost <port> < input.txt
- */
-#include <unistd.h>
 #include <iostream>
-#include <stdlib.h>
-#include <string>
-#include <sstream>
 #include <cstring>
+#include <sstream>
+#include <unistd.h>
 #include <pthread.h>
+#include <netinet/in.h>
 #include <sys/socket.h>
-#include <netinet/ip.h>
-#include <arpa/inet.h>
-#include <bits/stdc++.h>
+#include <unordered_map>
+
 using namespace std;
 
-unordered_map<string, string> KV_data;
+// Handle individual client connections
+int handleConnection(int &);
 
-void process_connection(int sockfd);
+// Create and configure server socket
+int getServerSocket(const int &port);
+
+unordered_map<string, string> KV_DATASTORE;
 
 int main(int argc, char **argv)
 {
-  int portno; /* port to listen on */
+	int port;
 
-  /*
-   * check command line arguments
-   */
-  if (argc != 2)
-  {
-    fprintf(stderr, "usage: %s <port>\n", argv[0]);
-    exit(1);
-  }
+	// Check command line arguments
+	if (argc != 2)
+	{
+		fprintf(stderr, "usage: %s <port>\n", argv[0]);
+		exit(1);
+	}
 
-  // DONE: Server port number taken as command line argument
-  portno = atoi(argv[1]);
-  std::cout << portno << std::endl;
-  int accept_sock, connection_sock;
-  struct sockaddr_in my_addr, client_addr;
+	// Server port number taken as command line argument
+	port = atoi(argv[1]);
 
-  connection_sock = socket(AF_INET, SOCK_STREAM, 0);
+	// Create server socket
+	int server_fd = getServerSocket(port);
 
-  my_addr.sin_family = AF_INET;
-  my_addr.sin_port = htons(portno);
-  bind(connection_sock, (struct sockaddr *)&my_addr, sizeof(struct sockaddr_in));
+	// Prepare to accept connections on socket FD.
+	if (listen(server_fd, 5) < 0)
+	{
+		cerr << "Error: Couldn't listen on socket" << endl;
+		close(server_fd);
+		return -1;
+	}
 
-  listen(connection_sock, 50);
+	cout << "Server listening on port: " << port << endl;
 
-  int client_len = sizeof(client_addr);
+	sockaddr_in client_addr;
+	socklen_t caddr_len = sizeof(client_addr);
 
-  while (true)
-  {
-    accept_sock = accept(connection_sock, (struct sockaddr *)&client_addr, (socklen_t *)&client_len);
-    process_connection(accept_sock);
-  }
-  
-  
-  close(connection_sock);
+	while (true)
+	{
+		// Await a connection on socket FD.
+		int client_fd = accept(server_fd, (sockaddr *)(&client_addr), &caddr_len);
+		if (client_fd < 0)
+		{
+			cerr << "Error: Couldn't accept connection" << endl;
+			exit(1);
+		}
+		handleConnection(client_fd);
+	}
+
+	// Close server file descriptor
+	close(server_fd);
+	return 0;
 }
 
-void process_connection(int sockfd)
+int getServerSocket(const int &port)
 {
-  char buffer[1024];
+	/* 	Creates a TCP socket and binds socket to specified
+		port.
+		Returns configured socket file descriptor.
+	 */
 
-  memset(buffer, 0, sizeof(buffer));
-  string additional = "";
-  string temp;
+	// TCP socket Creation and Configuration
 
-  while (true)
-  {
-    read(sockfd, buffer, 1024);
-    istringstream streamstring(additional + (string)buffer);
+	// Server socket file descriptor
+	int server_fd = socket(AF_INET, SOCK_STREAM, 0);
 
-    while (true)
-    {
-      streamstring >> temp;
-      if (temp == "READ")
-      {
-        // cout <<"READ" <<endl;
-        streamstring >> temp;
-        if (KV_data.find(temp) == KV_data.end())
-        {
-          const char *sendd = "NULL\n";
-          write(sockfd, sendd, strlen(sendd));
-        }
-        else
-        {
+	if (server_fd < 0)
+	{
+		cerr << "Error: Couldn't open socket" << endl;
+		return -1;
+	}
 
-          string se = KV_data[temp] + "\n";
-          const char *sendd = se.c_str();
-          write(sockfd, sendd, strlen(sendd));
-        }
-      }
-      else if (temp == "WRITE")
-      {
-        streamstring >> temp;
-        string key = temp;
-        streamstring >> temp;
-        temp = temp.substr(1, temp.length() - 1);
-        KV_data[key] = temp;
-        const char *sendd = "FIN\n";
-        write(sockfd, sendd, strlen(sendd));
-      }
-      else if (temp == "COUNT")
-      {
-        // cout <<"COUNT" <<endl;
-        int count = KV_data.size();
-        string se = to_string(count) + "\n";
-        const char *sendd = se.c_str();
-        write(sockfd, sendd, strlen(sendd));
-      }
-      else if (temp == "DELETE")
-      {
-        // cout <<"DELETE" <<endl;
-        streamstring >> temp;
-        if (KV_data.find(temp) == KV_data.end())
-        {
+	// Structure to store configuration details
+	struct sockaddr_in server_addr;
+	socklen_t saddr_len = sizeof(server_addr);
 
-          const char *sendd = "NULL\n";
-          write(sockfd, sendd, strlen(sendd));
-        }
-        else
-        {
-          KV_data.erase(temp);
-          const char *sendd = "FIN\n";
-          write(sockfd, sendd, strlen(sendd));
-        }
-      }
-      else if (temp == "END")
-      {
-        write(sockfd , "\n", strlen("\n"));
-        close(sockfd);
-        return;
-      }
-      else
-      {
-        if (streamstring.str().length() < 1)
-        {
-          additional = temp;
-          break;
-        }
-      }
+	memset(&server_addr, 0, saddr_len);
 
-      if (streamstring.str().length() < 1)
-      {
-        additional = "";
-        break;
-      }
+	server_addr.sin_family = AF_INET;
+	server_addr.sin_addr.s_addr = INADDR_ANY;
+	server_addr.sin_port = htons(port);
 
-      cout << temp << endl;
-    }
-  }
+	// Bind the socket to the address and port number
+	if (bind(server_fd, (struct sockaddr *)&server_addr, saddr_len) < 0)
+	{
+		cerr << "Error: Couldn't bind socket" << endl;
+		close(server_fd);
+		return -1;
+	}
+
+	return server_fd;
+}
+
+int handleConnection(int &client_fd)
+{
+	/* 	Handle Individual client connections and process
+		and respond to messages sent by the client.
+	*/
+
+	// Buffer to read in messages from client
+	char buffer[1024];
+
+	bool end = false;
+	string response;
+	string key, value;
+
+	// Until client sends END message
+	while (!end)
+	{
+		memset(buffer, 0, sizeof(buffer));
+		int bytesReceived = recv(client_fd, buffer, sizeof(buffer), 0);
+		if (bytesReceived < 0)
+		{
+			cerr << "Error: Couldn't receive message" << endl;
+			exit(1);
+		}
+		else if (bytesReceived == 0)
+		{
+			cout << "Client disconnected." << endl;
+			break;
+		}
+		else
+		{
+			string query;
+			stringstream strm(buffer);
+			while (getline(strm, query))
+			{
+
+				if (query == "READ")
+				{
+					// READ query
+
+					getline(strm, key);
+
+					// Check for presence of key
+					if (KV_DATASTORE.find(key) != KV_DATASTORE.end())
+					{
+						response = KV_DATASTORE[key] + "\n";
+					}
+					else
+					{
+						// Return NULL if key not present
+						response = "NULL\n";
+					}
+				}
+				else if (query == "WRITE")
+				{
+					// WRITE Query
+
+					getline(strm, key);
+					getline(strm, value);
+
+					// Strip colon
+					value = value.substr(1);
+
+					KV_DATASTORE[key] = value;
+
+					response = "FIN\n";
+				}
+				else if (query == "COUNT")
+				{
+					// COUNT query
+					response = to_string(KV_DATASTORE.size()) + "\n";
+				}
+				else if (query == "DELETE")
+				{
+					// DELETE query
+
+					getline(strm, key);
+					int count = 0;
+
+					// Check for presence of key
+					if (KV_DATASTORE.find(key) != KV_DATASTORE.end())
+					{
+						KV_DATASTORE.erase(key);
+						response = "FIN\n";
+					}
+					else
+					{
+						// Return NULL if key not present
+						response = "NULL\n";
+					}
+				}
+				else if (query == "END")
+				{
+					// End the connection
+					end = true;
+					break;
+				}
+
+				// Send response to client
+				send(client_fd, response.c_str(), response.length(), 0);
+
+				// Erase strings
+				response.clear();
+				key.clear();
+				value.clear();
+			}
+		}
+	}
+
+	// Close socket file descriptor
+	int res = close(client_fd);
+	return res;
 }
